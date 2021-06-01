0123b77a0503a8f66e62fff2eedfcb0e1724a579
Josh Jacobson <joshhjacobson@gmail.com>
2021-06-01 15:06:47 -0400

    Updating tasks so they default to have a todo on the particular day

--- gen_weekly.py
+++ gen_weekly.py
@@ -1,8 +1,13 @@
 import datetime
-
-DAYS_BACK_RETRO_REVIEW = [1,7,30]
+DAYS_BACK_RETRO_REVIEW = {
+    "Yesterday": 1,
+    "One Week": 7,
+    "One Month": 30
+}
 WEEKLY_TEMPLATE = "weekly_template.txt"
 OUTPUT_FOLDER = "weeks/"
+DAILY_TASKS = "daily_tasks.txt"
+TASK_LINE_TAG = "#@scheduled"
 
 def write_week_file():
     next_sunday = next_weekday(datetime.datetime.now(),6)
@@ -35,27 +40,46 @@ def get_week(start_date):
     return [start_date + datetime.timedelta(days=i) for i in range(7)]
 
 def process_date(datetime_obj):
-    date_line = "- [[" + roam_strftime(datetime_obj) + "]]"
+    date_line = "- " + roam_strftime(datetime_obj)
     body = get_date_body(datetime_obj)
     return date_line + "\r\n" + body
 
 def get_date_body(datetime_obj):
-    retros = get_retro_dates(datetime_obj)
-    date_file = open(WEEKLY_TEMPLATE).read().format(
-        yesterday = retros[0], 
-        one_week = retros[1], 
-        one_month = retros[2])
-    return date_file
+    date_file = open(WEEKLY_TEMPLATE).read()
+    return date_file + get_retro_section(datetime_obj) + get_tasks_section(datetime_obj)
 
-def get_retro_dates(datetime_obj):
-    return [roam_strftime(datetime_obj - datetime.timedelta(days = i)) for i in DAYS_BACK_RETRO_REVIEW]
+def get_retro_section(datetime_obj):
+    retro_section = "\r\n  - #[[Daily Notes Retro]]"
+    prefix = "\r\n      - "
+    for days_back_title in DAYS_BACK_RETRO_REVIEW:
+        roam_date_i = get_retro_date(datetime_obj,DAYS_BACK_RETRO_REVIEW[days_back_title])
+        line = prefix + days_back_title + ": " + roam_date_i
+        retro_section += line
+    return retro_section
+
+def get_tasks_section(datetime_obj):
+    roam_date = roam_strftime(datetime_obj)
+    task_section = "\r\n  - #Tasks"
+    prefix = "\r\n      - "
+    task_f = open(DAILY_TASKS, 'r')
+    lines = task_f.readlines()
+    for line in lines:
+        task_line = prefix + "{{[[TODO]]}} " + roam_date + " " + line.rstrip() + " " + TASK_LINE_TAG
+        task_section += task_line
+    return task_section
+
+
+def get_retro_date(datetime_obj,days_back):
+    return roam_strftime(datetime_obj - datetime.timedelta(days = days_back))
 
 def roam_strftime(datetime_obj):
-    return datetime_obj.strftime('%B {S}, %Y').replace('{S}', str(datetime_obj.day) + suffix(datetime_obj.day))
+    return "[[" + datetime_obj.strftime('%B {S}, %Y').replace('{S}', str(datetime_obj.day) + suffix(datetime_obj.day)) + "]]"
 
 def suffix(datetime_obj):
     return 'th' if 11<=datetime_obj<=13 else {1:'st',2:'nd',3:'rd'}.get(datetime_obj%10, 'th')
 
+#Quick tests
 #print(process_date(datetime.datetime(2021, 6, 1, 12, 27, 17, 277602)))
+#print(get_retro_section(datetime.datetime(2021, 6, 1, 12, 27, 17, 277602)))
 
 write_week_file()
